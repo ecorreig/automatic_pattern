1afb9a95c304fd568e46f31f95c2aa369071f161
Jaume Singla <jaume.singlavalls@gmail.com>
2015-10-02 19:04:42 +0200

    Changed the url for the get attachments script

--- attachments.py
+++ attachments.py
@@ -17,7 +17,7 @@ def main():
         except:
             pass
         writer.writerow([category.encode('UTF-8', 'replace'), attachment.name.encode('UTF-8', 'replace'),
-                         "/comment/attachment/"+attachment.id, attachment.description.encode('UTF-8', 'replace')])
+                         "/ca/comentaris/attachment/"+attachment.id, attachment.description.encode('UTF-8', 'replace')])
 
 
 if __name__=='__main__':
