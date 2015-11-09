35a3fb8569f572b0a3e87d82d7b29c3a1dc70247
Jaume Singla <jaume.singlavalls@gmail.com>
2015-11-09 12:43:39 +0100

    Added argparser to generalize and manage better the testing.

--- debug.py
+++ debug.py
@@ -4,8 +4,9 @@ import models
 import main as program
 from Api.EndpointModel import Model
 from datetime import datetime, date
-from dateutil import parser
+from dateutil import parser as date_parser
 from Api.Manager import DataManager
+import argparse
 
 
 def mock_save(self):
@@ -13,7 +14,6 @@ def mock_save(self):
 
 
 def debug(config_id):
-    # Model.save = mock_save
 
     program.load_cache()
 
@@ -26,14 +26,32 @@ def debug(config_id):
 
 
 def debug_main(day=date.today()):
-    Model.save = mock_save
     program.load_cache()
 
     program.main(day)
 
 
 if __name__ == "__main__":
-    # DataManager.sharedManager().set_config('config-beta.json')
-    # debug("14")
+    parser = argparse.ArgumentParser()
 
-    debug_main(parser.parse("2015/11/09"))
+    parser.add_argument('-d', default=None, help="debug a configuration", metavar=("id_config",))
+    parser.add_argument('-m', default=None, help="Run main configuration for a day", metavar=("day",))
+    parser.add_argument('-c', default=None, help="Use file configuration", metavar=("config.json",))
+    parser.add_argument('-s', default=False, action='store_true', help='Save the changes to the server, by default don\'t save it')
+
+    args = parser.parse_args()
+    if args.d and args.m:
+        parser.error("Argument -m and -d are mutual exclusives")
+
+    if args.c:
+        DataManager.sharedManager().set_config(args.c)
+
+    if not args.s:
+        Model.save = mock_save
+
+    if args.d:
+        debug(args.d)
+    elif args.m:
+        debug_main(date_parser.parse(args.m))
+    else:
+        parser.error("You should select -m or -d options to run")
